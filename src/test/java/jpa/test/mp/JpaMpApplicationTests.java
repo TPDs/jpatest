60b44ddfb68d8c84dcb5a2f349520424e780a126
Michael Petersen <Mig.mic@gmail.com>
2020-11-05 11:19:42 +0100

    Initial commit

---
+++ src/test/java/jpa/test/mp/JpaMpApplicationTests.java
@@ -0,0 +1,13 @@
+package jpa.test.mp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaMpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
